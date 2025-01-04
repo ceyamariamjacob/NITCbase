159c057c8c4fd75c2e034867c08286ee33c629ae
ceyamariamjacob <ceyajacob1@gmail.com>
2025-01-05 03:13:19 +0530

    initial commit

---
+++ NITCbase/mynitcbase/main.cpp
@@ -0,0 +1,69 @@
+
+#include "Buffer/StaticBuffer.h"
+#include "Buffer/BlockBuffer.h"
+#include "Cache/OpenRelTable.h"
+#include "Disk_Class/Disk.h"
+#include "FrontendInterface/FrontendInterface.h"
+#include <iostream>
+#include <cstring>
+using std::cout;
+using std::endl;
+
+/*int main(int argc, char *argv[]) {
+  /* Initialize the Run Copy of Disk 
+  Disk disk_run;
+  // StaticBuffer buffer;
+  // OpenRelTable cache;
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer,7000);
+  char message[]="hello";
+  memcpy(buffer+20,message,6);
+  Disk::writeBlock(buffer,7000);
+  
+  unsigned char buffer2[BLOCK_SIZE];
+  char message2[6];
+  Disk::readBlock(buffer2,7000);
+  memcpy(message2,buffer2+20,6);
+  std::cout<<message2;
+  return 0;
+  return FrontendInterface::handleFrontend(argc, argv);
+
+}*/
+
+int main(int argc, char *argv[]) {
+  Disk disk_run;
+
+  // create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
+
+  // load the headers of both the blocks into relCatHeader and attrCatHeader.
+  // (we will implement these functions later)
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  for (/* i = 0 to total relation count */) {
+
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    for (/* j = 0 to number of entries in the attribute catalog */) {
+
+      // declare attrCatRecord and load the attribute catalog entry into it
+
+      if (/* attribute catalog entry corresponds to the current relation */) {
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+        printf("  %s: %s\n", /* get the attribute name */, attrType);
+      }
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
