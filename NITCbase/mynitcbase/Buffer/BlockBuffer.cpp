273d61c777b5e28ac1c9303544fcc783259d580c
ceyamariamjacob <ceyajacob1@gmail.com>
2025-01-05 19:17:47 +0530

    Stage 2

--- NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
+++ NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
@@ -2,3 +2,76 @@
 
 #include <cstdlib>
 #include <cstring>
+
+// the declarations for these functions can be found in "BlockBuffer.h"
+
+BlockBuffer::BlockBuffer(int blockNum) {
+  // initialise this.blockNum with the argument
+  this->blockNum=blockNum;
+}
+
+// calls the parent class constructor
+RecBuffer::RecBuffer(int blockNum) : BlockBuffer::BlockBuffer(blockNum) {}
+
+// load the block header into the argument pointer
+int BlockBuffer::getHeader(struct HeadInfo *head) {
+  unsigned char buffer[BLOCK_SIZE];
+
+  // read the block at this.blockNum into the buffer
+  if(Disk::readBlock(buffer,this->blockNum) !=SUCCESS){return FAILURE;};
+  
+  // populate the numEntries, numAttrs and numSlots fields in *head
+  memcpy(&head->numSlots, buffer + 24, 4);
+  memcpy(&head->numEntries, buffer+16, 4);
+  memcpy(&head->numAttrs, buffer+20, 4);
+  memcpy(&head->rblock, buffer+12, 4);
+  memcpy(&head->lblock, buffer+8, 4);
+
+  return SUCCESS;
+}
+
+// load the record at slotNum into the argument pointer
+int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
+  struct HeadInfo head;
+
+  // get the header using this.getHeader() function
+  if(this->getHeader(&head)!=SUCCESS){return FAILURE;}
+
+  int attrCount = head.numAttrs;
+  int slotCount = head.numSlots;
+
+  // read the block at this.blockNum into a buffer
+  unsigned char buffer[BLOCK_SIZE];
+  
+  if(Disk::readBlock(buffer,this->blockNum)!=SUCCESS){return FAILURE;}
+  
+
+  /* record at slotNum will be at offset HEADER_SIZE + slotMapSize + (recordSize * slotNum)
+     - each record will have size attrCount * ATTR_SIZE
+     - slotMap will be of size slotCount
+  */
+  int recordSize = attrCount * ATTR_SIZE;
+  int slotMapSize= slotCount;
+  unsigned char *slotPointer = buffer+ HEADER_SIZE+slotMapSize+(recordSize*slotNum);
+
+  // load the record into the rec data structure
+  memcpy(rec, slotPointer, recordSize);
+
+  return SUCCESS;
+}
+/*
+int RecBuffer::loadBlockAndGetBuffer(unsigned char **buffPtr){
+  int bufferNum=StaticBuffer:getBufferNum(this->blockNum);
+  if(bufferNum==E_OUTOFBOUND){return E_OUTOFBOUND;}
+  
+  if(nufferNum!=E_BLOCKNOTINBUFFER){
+  	for(int bufferIdx=0; bufferIdx=BUFFER_CAPACITY;bufferIdx++){
+  		StaticBuffer::metainfo[bufferIdx].timeStamp++;
+  	}
+  	StaticBuffer::metainfo[bufferNum].timeStamp=0;
+  }
+  else if(bufferNum== E_BLOCKNOTINBUFFER){
+  	bufferNum=StaticBuffer::getFreeBuffer(this->blockNum);
+  	
+  } 
+}*/
--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -44,23 +44,30 @@ int main(int argc, char *argv[]) {
   // (we will implement these functions later)
   relCatBuffer.getHeader(&relCatHeader);
   attrCatBuffer.getHeader(&attrCatHeader);
-
-  for (/* i = 0 to total relation count */) {
-
+  
+  for (int i=0;i<relCatHeader.numEntries;i++) {
+    int  attrCatSlotIndex = 0;
     Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
-
     relCatBuffer.getRecord(relCatRecord, i);
 
     printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
-
-    for (/* j = 0 to number of entries in the attribute catalog */) {
+    
+    for (int j=0;j<attrCatHeader.numEntries;j++) {
 
       // declare attrCatRecord and load the attribute catalog entry into it
-
-      if (/* attribute catalog entry corresponds to the current relation */) {
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer.getRecord(attrCatRecord,j);
+      if (strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal, attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal)==0) {
         const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
-        printf("  %s: %s\n", /* get the attribute name */, attrType);
+        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
       }
+      
+      if(attrCatSlotIndex == attrCatHeader.numSlots-1){
+      	attrCatSlotIndex=-1;
+      	attrCatBuffer=RecBuffer(attrCatHeader.rblock);
+      	attrCatBuffer.getHeader(&attrCatHeader);
+      }else{
+      attrCatSlotIndex++;}
     }
     printf("\n");
   }
