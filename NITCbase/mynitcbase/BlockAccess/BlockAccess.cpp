846fd252e9addbeb1229ea147c5ef56f15e0b941
ceyamariamjacob <ceyajacob1@gmail.com>
2025-03-23 16:14:21 +0530

    Stage 10 done w/o ex

--- NITCbase/mynitcbase/BlockAccess/BlockAccess.cpp
+++ NITCbase/mynitcbase/BlockAccess/BlockAccess.cpp
@@ -479,7 +479,7 @@ int BlockAccess::insert(int relId, Attribute *record) {
 NOTE: This function will copy the result of the search to the `record` argument.
       The caller should ensure that space is allocated for `record` array
       based on the number of attributes in the relation.
-*/
+
 int BlockAccess::search(int relId, Attribute *record, char attrName[ATTR_SIZE], Attribute attrVal, int op) {
     // Declare a variable called recid to store the searched record
     RecId recId;
@@ -487,7 +487,7 @@ int BlockAccess::search(int relId, Attribute *record, char attrName[ATTR_SIZE], 
 
     /* search for the record id (recid) corresponding to the attribute with
     attribute name attrName, with value attrval and satisfying the condition op
-    using linearSearch() */
+    using linearSearch() 
     if(recId.block==-1 and recId.slot==-1){
         return E_NOTFOUND;
     }
@@ -497,7 +497,7 @@ int BlockAccess::search(int relId, Attribute *record, char attrName[ATTR_SIZE], 
     /* Copy the record with record id (recId) to the record buffer (record)
        For this Instantiate a RecBuffer class object using recId and
        call the appropriate method to fetch the record
-    */
+    
    RecBuffer recBuffer(recId.block);
    int ret=recBuffer.getRecord(record,recId.slot);
    if(ret!=SUCCESS){
@@ -507,14 +507,7 @@ int BlockAccess::search(int relId, Attribute *record, char attrName[ATTR_SIZE], 
     return SUCCESS;
 }
 
-
-
-
-
-
-
-
-
+*/
 
 int BlockAccess::deleteRelation(char relName[ATTR_SIZE]) {
 
@@ -823,4 +816,56 @@ int BlockAccess::project(int relId, Attribute *record) {
    recordBlockBuffer.getRecord(record,nextRecId.slot);
 
    return SUCCESS;
+}
+
+int BlockAccess::search(int relId, Attribute *record, char attrName[ATTR_SIZE], Attribute attrVal, int op) {
+    // Declare a variable called recid to store the searched record
+    RecId recId;
+
+    /* get the attribute catalog entry from the attribute cache corresponding
+    to the relation with Id=relid and with attribute_name=attrName  */
+    AttrCatEntry attrCatEntry;
+    int ret=AttrCacheTable::getAttrCatEntry(relId,attrName,&attrCatEntry);
+    // if this call returns an error, return the appropriate error code
+    if(ret!=SUCCESS){
+        return ret;
+    }
+
+    // get rootBlock from the attribute catalog entry
+    int rootBlock=attrCatEntry.rootBlock;
+    /* if Index does not exist for the attribute (check rootBlock == -1) */ 
+    if(rootBlock==-1){
+
+        /* search for the record id (recid) corresponding to the attribute with
+           attribute name attrName, with value attrval and satisfying the
+           condition op using linearSearch()
+        */
+       recId=BlockAccess::linearSearch(relId,attrName,attrVal,op);
+    }
+
+    /* else */ 
+    else{
+        // (index exists for the attribute)
+
+        /* search for the record id (recid) correspoding to the attribute with
+        attribute name attrName and with value attrval and satisfying the
+        condition op using BPlusTree::bPlusSearch() */
+        recId=BPlusTree::bPlusSearch(relId,attrName,attrVal,op);
+    }
+
+
+    // if there's no record satisfying the given condition (recId = {-1, -1})
+    //     return E_NOTFOUND;
+    if(recId.block==-1 or recId.slot==-1){
+        return E_NOTFOUND;
+    }
+
+    /* Copy the record with record id (recId) to the record buffer (record).
+       For this, instantiate a RecBuffer class object by passing the recId and
+       call the appropriate method to fetch the record
+    */
+    RecBuffer recordBlock(recId.block);
+    recordBlock.getRecord(record,recId.slot);
+
+    return SUCCESS;
 }
--- NITCbase/mynitcbase/Cache/AttrCacheTable.cpp
+++ NITCbase/mynitcbase/Cache/AttrCacheTable.cpp
@@ -77,3 +77,111 @@ void AttrCacheTable::attrCatEntryToRecord(AttrCatEntry *attrCatEntry, Attribute 
     // copy the rest of the fields in the record to the attrCacheEntry struct
 }
 
+int AttrCacheTable::getSearchIndex(int relId, char attrName[ATTR_SIZE], IndexId *searchIndex) {
+
+	if(relId<0 or relId>=MAX_OPEN) {
+	  return E_OUTOFBOUND;
+	}
+  
+	if(attrCache[relId]==nullptr) {
+	  return E_RELNOTOPEN;
+	}
+  
+	for(AttrCacheEntry *entry=attrCache[relId];entry!=nullptr;entry=entry->next)
+	{
+	  if (strcmp(entry->attrCatEntry.attrName,attrName)==0)
+	  {
+		//copy the searchIndex field of the corresponding Attribute Cache entry
+		//in the Attribute Cache Table to input searchIndex variable.
+		*searchIndex=entry->searchIndex;
+		return SUCCESS;
+	  }
+	}
+  
+	return E_ATTRNOTEXIST;
+  }
+  int AttrCacheTable::getSearchIndex(int relId,int attrOffset, IndexId *searchIndex) {
+
+	if(relId<0 or relId>=MAX_OPEN) {
+	  return E_OUTOFBOUND;
+	}
+  
+	if(attrCache[relId]==nullptr) {
+	  return E_RELNOTOPEN;
+	}
+  
+	for(AttrCacheEntry *entry=attrCache[relId];entry!=nullptr;entry=entry->next)
+	{
+	  if (entry->attrCatEntry.offset==attrOffset)
+	  {
+		//copy the searchIndex field of the corresponding Attribute Cache entry
+		//in the Attribute Cache Table to input searchIndex variable.
+		*searchIndex=entry->searchIndex;
+		return SUCCESS;
+	  }
+	}
+  
+	return E_ATTRNOTEXIST;
+  }
+  int AttrCacheTable::setSearchIndex(int relId, char attrName[ATTR_SIZE], IndexId *searchIndex) {
+
+	if(relId<0 or relId>=MAX_OPEN) {
+	  return E_OUTOFBOUND;
+	}
+  
+	if(attrCache[relId]==nullptr) {
+	  return E_RELNOTOPEN;
+	}
+  
+	for(AttrCacheEntry *entry=attrCache[relId];entry!=nullptr;entry=entry->next)
+	{
+	  if (strcmp(entry->attrCatEntry.attrName,attrName)==0)
+	  {
+		// copy the input searchIndex variable to the searchIndex field of the
+		//corresponding Attribute Cache entry in the Attribute Cache Table.
+		entry->searchIndex=*searchIndex;
+		return SUCCESS;
+	  }
+	}
+  
+	return E_ATTRNOTEXIST;
+  }
+  int AttrCacheTable::setSearchIndex(int relId, int attrOffset, IndexId *searchIndex) {
+
+	if(relId<0 or relId>=MAX_OPEN) {
+	  return E_OUTOFBOUND;
+	}
+  
+	if(attrCache[relId]==nullptr) {
+	  return E_RELNOTOPEN;
+	}
+  
+	for(AttrCacheEntry *entry=attrCache[relId];entry!=nullptr;entry=entry->next)
+	{
+	  if (entry->attrCatEntry.offset==attrOffset)
+	  {
+		// copy the input searchIndex variable to the searchIndex field of the
+		//corresponding Attribute Cache entry in the Attribute Cache Table.
+		entry->searchIndex=*searchIndex;
+		return SUCCESS;
+	  }
+	}
+  
+	return E_ATTRNOTEXIST;
+  }
+  int AttrCacheTable::resetSearchIndex(int relId, char attrName[ATTR_SIZE]) {
+
+	// declare an IndexId having value {-1, -1}
+	// set the search index to {-1, -1} using AttrCacheTable::setSearchIndex
+	// return the value returned by setSearchIndex
+	IndexId searchIndex={-1,-1};
+	return setSearchIndex(relId,attrName,&searchIndex);
+  }
+  int AttrCacheTable::resetSearchIndex(int relId, int attrOffset) {
+
+	// declare an IndexId having value {-1, -1}
+	// set the search index to {-1, -1} using AttrCacheTable::setSearchIndex
+	// return the value returned by setSearchIndex
+	IndexId searchIndex={-1,-1};
+	return setSearchIndex(relId,attrOffset,&searchIndex);
+  }
